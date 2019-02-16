b4886798fca296488045ee65b3a7fc5e83132620
NFS101 <realnfs101@gmail.com>
2019-02-16 12:27:34 +0600

    Updated Map

--- src/draw_map.py
+++ src/draw_map.py
@@ -4,7 +4,7 @@ class DrawMap:
     def draw_map(self):
         stdscr = curses.initscr()
 
-        stdscr.addstr(0 , 0 , "####################")
+        stdscr.addstr(0 , 0 , "████████████████████")
         for i in range(1 , 9):
-            stdscr.addstr(i , 0 , "#                  #")
-        stdscr.addstr(9 , 0 , "####################")
+            stdscr.addstr(i , 0 , "█                  █")
+        stdscr.addstr(9 , 0 , "████████████████████")
